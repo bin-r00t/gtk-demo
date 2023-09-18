12e6491847c1f4b9e1fb6369dd6bd45f1107c220
liubin <bh0bama@sina.com>
2023-09-17 23:52:46 -0700

    update exampleapp

--- 1.trivial-application/main.c
+++ 1.trivial-application/main.c
@@ -1,9 +1,7 @@
 #include <gtk/gtk.h>
-#include "trivial-application.h"
+#include "exampleapp.h"
 
-
-int
-main (int argc, char *argv[])
+int main(int argc, char *argv[])
 {
-    return g_application_run (G_APPLICATION (example_app_new()), argc, argv);
+    return g_application_run(G_APPLICATION(example_app_new()), argc, argv);
 }
